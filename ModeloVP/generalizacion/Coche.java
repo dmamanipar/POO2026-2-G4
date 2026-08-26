122d74f764b617c1d6ee657bd6b3f435fef09a63
dmamanipar <dmamanipar@unsa.edu.pe>
2026-08-26 11:55:12 -0500

    Solo se genero diagrmas de codigo-falta corregir

---
+++ ModeloVP/generalizacion/Coche.java
@@ -0,0 +1,12 @@
+package generalizacion;
+
+public class Coche extends Vehiculo {
+
+	private int numeroPuertas;
+
+	public void abrirMaletero() {
+		// TODO - implement Coche.abrirMaletero
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ ModeloVP/generalizacion/Vehiculo.java
@@ -0,0 +1,28 @@
+package generalizacion;
+
+public class Vehiculo {
+
+	private String marca;
+	private int velocidadMaxima;
+
+	/**
+	 * 
+	 * @param marca
+	 * @param velocidadMaxima
+	 */
+	public Vehiculo(String marca, int velocidadMaxima) {
+		// TODO - implement Vehiculo.Vehiculo
+		throw new UnsupportedOperationException();
+	}
+
+	public void acelerar() {
+		// TODO - implement Vehiculo.acelerar
+		throw new UnsupportedOperationException();
+	}
+
+	public void frenar() {
+		// TODO - implement Vehiculo.frenar
+		throw new UnsupportedOperationException();
+	}
+
+}
