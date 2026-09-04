ba4f880e9d6221a3fdf95500c63983dd76f48d87
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-04 15:03:27 -0500

    Proyecto Inicial

--- AlgotirmoGUI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.algotirmogui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.algotirmogui to javafx.fxml;
-    exports pe.edu.upeu.algotirmogui;
-}
