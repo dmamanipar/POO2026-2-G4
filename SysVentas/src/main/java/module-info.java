30971e5d7acc7746445cb35bd7a047c006d4e0ce
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-16 10:22:15 -0500

    Avance Controller

--- SysVentas/src/main/java/module-info.java
+++ SysVentas/src/main/java/module-info.java
@@ -8,5 +8,6 @@ module pe.edu.upeu.sysventas {
     requires jakarta.validation;
 
     opens pe.edu.upeu.sysventas to javafx.fxml;
+    opens pe.edu.upeu.sysventas.controller to javafx.fxml;
     exports pe.edu.upeu.sysventas;
 }
--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
@@ -2,16 +2,24 @@ package pe.edu.upeu.sysventas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import pe.edu.upeu.sysventas.config.AppContext;
 
 import java.io.IOException;
 
 public class SysVentas extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SysVentas.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        AppContext appContext=AppContext.getInstance();
+        Screen screen=Screen.getPrimary();
+        FXMLLoader fxmlLoader = new FXMLLoader(SysVentas.class.getResource("/view/main_producto.fxml"));
+        fxmlLoader.setControllerFactory(appContext::getBean);
+        Rectangle2D dimension=screen.getBounds();
+
+        Scene scene = new Scene(fxmlLoader.load(), dimension.getWidth(), dimension.getHeight()-60);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
