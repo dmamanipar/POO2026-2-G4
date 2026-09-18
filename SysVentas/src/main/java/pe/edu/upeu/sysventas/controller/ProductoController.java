278b74437e0bfaff903798db42a64038ecb47287
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-17 19:00:58 -0500

    Avance interfaz

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/controller/ProductoController.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/controller/ProductoController.java
@@ -1,6 +1,10 @@
 package pe.edu.upeu.sysventas.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import lombok.RequiredArgsConstructor;
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
+import pe.edu.upeu.sysventas.enums.TipoProducto;
 import pe.edu.upeu.sysventas.service.ICategoriaService;
 import pe.edu.upeu.sysventas.service.IMarcaService;
 import pe.edu.upeu.sysventas.service.IProductoService;
@@ -12,5 +16,15 @@ public class ProductoController {
     private final ICategoriaService cs;
     private final IProductoService ps;
     private final IUnidadMedidaService us;
+
+    @FXML
+    ComboBox<ComboBoxOption> cbxTipoProducto, cbxMarca, cbxCategoria;
+
+    @FXML public void initialize(){
+        cbxTipoProducto.getItems().addAll(ps.listarTipoProducto());
+        cbxMarca.getItems().addAll(ms.listarCombobox());
+        cbxCategoria.getItems().addAll(cs.listarCombobox());
+    }
+
 
 }
